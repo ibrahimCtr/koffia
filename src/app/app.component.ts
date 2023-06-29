67f8876771e52b407f62da4b4a34a95bb8564903
Ramazan Alp <ralp@cfcunderwriting.com>
2023-06-29 10:12:54 +0100

    koffia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'koffia';
+	title = "koffia";
+	menuItem = [
+		{ name: "galatoIceCream", image: "../../assets/gelato-ice-cream.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+		{ name: "Home", image: "../assets/menu.png", price: "1000" },
+	];
 }
